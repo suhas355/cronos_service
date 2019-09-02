34836e7e785606fff2302187b1d7d47661d3aad5
shwethag <g.shwetha.g@gmail.com>
2019-09-02 19:41:13 +0530

    Added models

--- cronos_service/api/admin.py
+++ cronos_service/api/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(
+    Profile,
+    list_display=['user', 'device_id'],
+    search_fields=['user', 'device_id']
+)
+
+admin.site.register(
+    Application,
+    list_display=['application_name', 'application_type'],
+    search_fields=['application_name', 'application_type']
+)
+
+admin.site.register(
+    Notification,
+    list_display=['profile', 'application', 'date', 'time', 'count'],
+    search_fields=['profile', 'application', 'date', 'time', 'count']
+)
+
--- cronos_service/api/models.py
+++ cronos_service/api/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    device_id = models.CharField(max_length=20, unique=True)
+
+    def __unicode__(self):
+        return u'%s %s' % self.user, self.device_id
+
+
+class Application(models.Model):
+    SOCIAL = 1
+    WORK = 2
+    MESSAGE = 3
+    OTHERS = 4
+    APP_TYPE_CHOICES = [
+        (SOCIAL, 'SOCIAL'),
+        (WORK, 'WORK'),
+        (MESSAGE, 'MESSAGE'),
+        (OTHERS, 'OTHERS')
+    ]
+    application_name = models.CharField(unique=True, max_length=100)
+    application_type = models.IntegerField(choices=APP_TYPE_CHOICES, default=OTHERS)
+
+    def __unicode__(self):
+        return u'%s' % self.application_name
+
+
+class Notification(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    application = models.ForeignKey(Application, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.DateTimeField()
+    count = models.IntegerField()
+
+    def __unicode__(self):
+        return u'%s %s' % self.profile.user.username, self.application.application_name
